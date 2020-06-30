58b67a1abd5c1ba6694879e5ab7a590f4ab31ba1
tmixell <tmixell@globalcloud.net>
2020-06-29 23:37:19 -0400

    DD-17408, DD-17410 restructured repo, updated readme, added streaming-alerts, added config.js

---
+++ config.js
@@ -0,0 +1,25 @@
+window.config = {
+
+	// the url of the DonorDrive API you wish to poll
+	api: 'https://docs.donordrive.com/api/',
+	// the resource (events, participants, teams)
+	resource: 'participants',
+	// the ID of the specific resource to poll
+	resourceID: 2065,
+
+	// properties specific to donation-ticker
+	donationTicker: {
+		// the duration (in seconds) the "new" badge will display next to a donation
+		highlightDuration: 60,
+		// donations over this amount will be highlighted in the list (null to ignore highlighting)
+		highlightThreshold: 100,
+		// the number of donations to return (1-100)
+		listLimit: 25
+	},
+
+	// properties specific to streaming-thermometer
+	streamingThermometer: {
+		showDonations: true
+	}
+
+};
--- streaming-overlay/README.md
+++
@@ -1,3 +0,0 @@
-## streaming-overlay
-
-streaming-overlay is intended to be leveraged inside your favorite broadcasting software. It leverages the same client-side libraries as [donation-ticker](../donation-ticker/README.md).
